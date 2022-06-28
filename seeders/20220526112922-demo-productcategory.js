54df5df9e680c85794a1f23530e798311fabad3c
Santiremix <santiagoguevara118@gmail.com>
2022-06-28 21:06:42 +0200

    mas seeders

--- seeders/20220526112922-demo-productcategory.js
+++ seeders/20220526112922-demo-productcategory.js
@@ -40,6 +40,12 @@ module.exports = {
         updatedAt: new Date()
       },
       {
+        CategoryId: [6],
+        ProductId: [3],      
+        createdAt: new Date(),
+        updatedAt: new Date()
+      },
+      {
         CategoryId: [1],
         ProductId: [4],       
         createdAt: new Date(),
@@ -111,9 +117,206 @@ module.exports = {
         createdAt: new Date(),
         updatedAt: new Date()
       },
+      {
+        CategoryId: [1],
+        ProductId: [11],    
+        createdAt: new Date(),
+        updatedAt: new Date()
+      },
+      {
+        CategoryId: [5],
+        ProductId: [11],    
+        createdAt: new Date(),
+        updatedAt: new Date()
+      },
+      {
+        CategoryId: [2],
+        ProductId: [12],    
+        createdAt: new Date(),
+        updatedAt: new Date()
+      },
+      {
+        CategoryId: [5],
+        ProductId: [12],    
+        createdAt: new Date(),
+        updatedAt: new Date()
+      },
+      {
+        CategoryId: [6],
+        ProductId: [12],    
+        createdAt: new Date(),
+        updatedAt: new Date()
+      },
+      {
+        CategoryId: [3],
+        ProductId: [13],    
+        createdAt: new Date(),
+        updatedAt: new Date()
+      },
+      {
+        CategoryId: [5],
+        ProductId: [13],    
+        createdAt: new Date(),
+        updatedAt: new Date()
+      },
+      {
+        CategoryId: [6],
+        ProductId: [13],    
+        createdAt: new Date(),
+        updatedAt: new Date()
+      },
+      {
+        CategoryId: [3],
+        ProductId: [14],    
+        createdAt: new Date(),
+        updatedAt: new Date()
+      },
+      {
+        CategoryId: [6],
+        ProductId: [14],    
+        createdAt: new Date(),
+        updatedAt: new Date()
+      },
+      {
+        CategoryId: [1],
+        ProductId: [15],    
+        createdAt: new Date(),
+        updatedAt: new Date()
+      },
+      {
+        CategoryId: [6],
+        ProductId: [15],    
+        createdAt: new Date(),
+        updatedAt: new Date()
+      },
+      {
+        CategoryId: [1],
+        ProductId: [16],    
+        createdAt: new Date(),
+        updatedAt: new Date()
+      },
+      {
+        CategoryId: [6],
+        ProductId: [16],    
+        createdAt: new Date(),
+        updatedAt: new Date()
+      },
+      {
+        CategoryId: [4],
+        ProductId: [17],    
+        createdAt: new Date(),
+        updatedAt: new Date()
+      },
+      {
+        CategoryId: [5],
+        ProductId: [17],    
+        createdAt: new Date(),
+        updatedAt: new Date()
+      },
+      {
+        CategoryId: [6],
+        ProductId: [17],    
+        createdAt: new Date(),
+        updatedAt: new Date()
+      },
+      {
+        CategoryId: [4],
+        ProductId: [18],    
+        createdAt: new Date(),
+        updatedAt: new Date()
+      },
+      {
+        CategoryId: [5],
+        ProductId: [18],    
+        createdAt: new Date(),
+        updatedAt: new Date()
+      },
+      {
+        CategoryId: [6],
+        ProductId: [18],    
+        createdAt: new Date(),
+        updatedAt: new Date()
+      },
+      {
+        CategoryId: [2],
+        ProductId: [19],    
+        createdAt: new Date(),
+        updatedAt: new Date()
+      },
+      {
+        CategoryId: [5],
+        ProductId: [19],    
+        createdAt: new Date(),
+        updatedAt: new Date()
+      },
+      {
+        CategoryId: [1],
+        ProductId: [20],    
+        createdAt: new Date(),
+        updatedAt: new Date()
+      },
+      {
+        CategoryId: [5],
+        ProductId: [20],    
+        createdAt: new Date(),
+        updatedAt: new Date()
+      },
+      {
+        CategoryId: [4],
+        ProductId: [21],    
+        createdAt: new Date(),
+        updatedAt: new Date()
+      },
+      {
+        CategoryId: [5],
+        ProductId: [21],    
+        createdAt: new Date(),
+        updatedAt: new Date()
+      },
+      {
+        CategoryId: [6],
+        ProductId: [21],    
+        createdAt: new Date(),
+        updatedAt: new Date()
+      },
+      {
+        CategoryId: [4],
+        ProductId: [22],    
+        createdAt: new Date(),
+        updatedAt: new Date()
+      },
+      {
+        CategoryId: [5],
+        ProductId: [22],    
+        createdAt: new Date(),
+        updatedAt: new Date()
+      },
+      {
+        CategoryId: [6],
+        ProductId: [22],    
+        createdAt: new Date(),
+        updatedAt: new Date()
+      },
+      {
+        CategoryId: [3],
+        ProductId: [23],    
+        createdAt: new Date(),
+        updatedAt: new Date()
+      },
+      {
+        CategoryId: [5],
+        ProductId: [23],    
+        createdAt: new Date(),
+        updatedAt: new Date()
+      },
+      {
+        CategoryId: [6],
+        ProductId: [23],    
+        createdAt: new Date(),
+        updatedAt: new Date()
+      },
   ], {});
   },
-
   async down (queryInterface, Sequelize) {
     /**
      * Add commands to revert seed here.
